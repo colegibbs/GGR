055e281acf9028370b1160af8e756fbf57a830fc
Cole Gibbs <colegibbs0@gmail.com>
2022-07-01 11:39:13 -0700

    inital commit

---
+++ G_G_R/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for G_G_R project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'G_G_R.settings')
+
+application = get_wsgi_application()
