60f9c21a92330081cbcc0e8d9d3d33513486e36f
Cole Gibbs <colegibbs0@gmail.com>
2022-07-05 13:47:13 -0700

    url paths for project level urls

--- G_G_R/urls.py
+++ G_G_R/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from .views import ListingList, ListingDetail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", ListingList.as_view(), name="listing_list"),
+    path("<int:pk>/", ListingDetail.as_view(), name="Listing_detail"),
 ]
